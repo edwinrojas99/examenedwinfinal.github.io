f140cba77d606b5d921bbb348f07681067058c80
EDWIN ROJAS <103547525+edwinrojas99@users.noreply.github.com>
2022-09-25 15:41:02 -0400

    Add files via upload

---
+++ paginas/funcion.js
@@ -0,0 +1,6 @@
+function dolares(valNum) {
+    document.getElementById("inputDolares").value=valNum*6.96;
+  }
+  function boliavanos(valNum) {
+    document.getElementById("inputBol").value=valNum*6.96;
+  }
